c1892a478a3375cc7f7f8c41f73c3471cb86880f
unknown <sonic-sonic99@mail.ru>
2020-03-21 13:05:58 +0300

    Fixed

--- Main.py
+++ Main.py
@@ -21,20 +21,13 @@ dct = {x: x+1 for x in range(len(line))}
 for i in range(len(line)):
     dct[i] = line[i]
 
-# for line in train_data:
-#     lst = []
-#     some = int(line[1][0])
-#     lst.append(some)
-#     print(lst)
-
 def preparation(str_matrix):
     f_matrix = []
-
     targets = []
 
     for line in str_matrix:
         target = np.zeros(out_n) + 0.1
-        target[int(line[0])] = 0.99
+        target[int(line[0] + line[1])] = 0.99
         targets.append(target)
         line = line.strip().split(',')
         data = (np.asfarray(line[1:]) / 1.0 * 0.99) + 0.1
@@ -53,8 +46,3 @@ for i in range(hid_n):
     correct_label = np.argmax(targets[i])
     predict_label = np.argmax(output)
     print("Прогноз - {}, Результат - {}".format(str(dct[correct_label]), str(dct[predict_label])))
-
-
-# pprint(n.w_ih)
-# pprint(n.w_ho)
-# print(np.argmax((n.query([1,1,1,1,0,0,1,1,0,1,0,1,1,1,1]))))
